3f70bbdaf7104ffc5856c74d8c6e275a45389a43
peterbrescia <peter.brescia@outlook.com>
2013-09-12 19:29:50 +0100

    adasdad

--- metacheckout.php
+++ metacheckout.php
@@ -21,8 +21,7 @@ if (isset($_POST['version']) && isset($_POST['password'])) {
   if ($checkoutPassword == $pas) {
     //if (!valid_git_branch($ver)) {
       $message  = 'Last ver = '.$lastver.'<br />';
-      $commands = 'eval `ssh-agent` 2>&1; ssh-add '.$githubKey.' 2>&1; cd '.$projectGithubDirectory.' 2>&1; git fetch github '.$ver.':'.$ver.' -v 2>&1;';
-      // git --work-tree='.$projectWorkingDirectory.' checkout -f '.$ver.' 2>&1';
+      $commands = 'eval `ssh-agent` 2>&1; ssh-add '.$githubKey.' 2>&1; cd '.$projectGithubDirectory.' 2>&1; git fetch -v github '.$ver.':'.$ver.' -v 2>&1; git --work-tree='.$projectWorkingDirectory.' checkout -f '.$ver.' 2>&1';
       $output = shell_exec($commands);
       $message .= str_replace(';', ';'.PHP_EOL, $commands).PHP_EOL;
       $message .= $output;

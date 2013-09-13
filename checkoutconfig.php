480dae722059344318b9b418c842f7e0df65e0fd
peterbrescia <peter.brescia@outlook.com>
2013-09-13 13:36:43 +0100

    asdasdsad

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -3,7 +3,7 @@
 	$githubKey = "/home/www-data/.ssh/nfcserverkey";
 	$projectGithubDirectory = "/home/ubuntu/nfc-members-card-server/ ";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/nfc-members-card-server/";
-	$github = "https://github.com/TobyAbel/nfc-members-card-server/";		//Github project location.
+	$github = "git@github.com:TobyAbel/nfc-members-card-server.git";		//Github project location.
 
 	$lastVersionFile = "/home/www-data/nfc-members-card-server/lastversion.php";
 ?>

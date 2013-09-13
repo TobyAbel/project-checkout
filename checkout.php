983a984bd5e01f6aaf36b3f423bc33ab0da1ad0d
peterbrescia <peter.brescia@outlook.com>
2013-09-13 13:40:33 +0100

    asdasd

--- checkout.php
+++ checkout.php
@@ -23,7 +23,7 @@ if (isset($_POST['version']) && isset($_POST['password'])) {
     // if $projectGithubDirectory does not contain a git repo
       // make one.
     if (!file_exists($projectGithubDirectory.'HEAD')) {// It's a silly hack but it doesn't need to be resilient
-      $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git clone --bare ".$github." 2>&1;";
+      $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git clone --bare ".$github." ./ 2>&1;";
       $output = shell_exec($commands);
       $message .= $commands.PHP_EOL.$output.PHP_EOL;
     }

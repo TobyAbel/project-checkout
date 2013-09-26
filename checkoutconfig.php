20576d2776f6721082fc682b372f036a61ca7288
peterbrescia <peter.brescia@outlook.com>
2013-09-26 17:40:43 +0100

    c5

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,7 +1,7 @@
 <?php
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/clonerepokey";
-	$projectGithubDirectory = "/home/ubuntu/clonedirectory/ ";		//Producct github directory
+	$projectGithubDirectory = "/home/ubuntu/clonedirectory/";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/clonedirectory/";
 	$github = "git@github.com:peterbrescia/clonerepo.git";		//Github project location.
 	$directory = "/home/www-data/clonedirectory";

1a826b8a7679e98953f4c1a9a49da9737311034c
peterbrescia <peter.brescia@outlook.com>
2013-09-26 17:04:24 +0100

    clone creation.

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,9 +1,9 @@
 <?php
 	$checkoutPassword = 111;
-	$githubKey = "/home/www-data/.ssh/nfcserverkey";
-	$projectGithubDirectory = "/home/ubuntu/nfc-members-card-server/ ";		//Producct github directory
-	$projectWorkingDirectory = "/var/www/nfc-members-card-server/";
-	$github = "git@github.com:TobyAbel/nfc-members-card-server.git";		//Github project location.
+	$githubKey = "/home/www-data/.ssh/clonerepokey";
+	$projectGithubDirectory = "/home/ubuntu/clonedirectory/ ";		//Producct github directory
+	$projectWorkingDirectory = "/var/www/clonedirectory/";
+	$github = "git@github.com:peterbrescia/clonerepo.git";		//Github project location.
 
-	$lastVersionFile = "/home/www-data/nfc-members-card-server/lastversion.php";
+	$lastVersionFile = "/home/www-data/clonedirectory/lastversion.php";
 ?>

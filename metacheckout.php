5543cd5309f906a1911766246a0c881917644acb
peterbrescia <peter.brescia@outlook.com>
2013-09-27 11:54:33 +0100

    12

--- metacheckout.php
+++ metacheckout.php
@@ -22,6 +22,7 @@ if (isset($_POST['version']) && isset($_POST['password'])) {
   if ($checkoutPassword == $pas) {
       $message  = 'Boom.'.PHP_EOL.'Last ver = '.$lastver.'<br />';
       $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git fetch github ".$ver.":".$ver." -v 2>&1; git --work-tree=".$projectWorkingDirectory." checkout -f ".$ver." 2>&1";
+      $message .= 'Placeholder 0';
       $output = shell_exec($commands);
       $message .= str_replace(';', ';'.PHP_EOL, $commands).PHP_EOL;
       $message .= $output;

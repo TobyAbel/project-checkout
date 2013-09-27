115cdddfe17511426888b29e01a61991e392acce
peterbrescia <peter.brescia@outlook.com>
2013-09-27 11:43:08 +0100

    5

--- checkout.php
+++ checkout.php
@@ -37,7 +37,6 @@ if (isset($_POST['version']) && isset($_POST['password'])) {
       // make one.
     if (!file_exists($projectGithubDirectory.'HEAD')) {// It's a silly hack but it doesn't need to be resilient
       $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git clone --bare ".$github." ./ 2>&1; git remote add github ".$github." 2>&1;";
-      shell_exec($commands);
       $output = shell_exec($commands);
       $message .= $commands.PHP_EOL.$output.PHP_EOL;
     }
--- metacheckoutconfig.php
+++ metacheckoutconfig.php
@@ -3,6 +3,6 @@
 	$githubKey = "/home/www-data/.ssh/checkoutkey";
 	$projectGithubDirectory = "/home/ubuntu/project-checkout/ ";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/project-checkout/";
-	$github = "https://github.com/TobyAbel/project-checkout/";		//Github project location.
+	$github = "https://github.com/TobyAbel/project-checkout";		//Github project location.
 	$lastVersionFile = "/home/www-data/project-checkout/lastversion.php";
 ?>

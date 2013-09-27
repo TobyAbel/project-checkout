f1c4e9263727c7675bd2fdf3f4151254b0fddd73
peterbrescia <peter.brescia@outlook.com>
2013-09-27 13:13:40 +0100

    23

--- metacheckoutconfig.php
+++ metacheckoutconfig.php
@@ -1,8 +1,9 @@
 <?php
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/checkoutkey";
-	$projectGithubDirectory = "/home/ubuntu/project-checkout/ ";		//Producct github directory
+	$projectCheckoutGithubDirectory = "/home/ubuntu/project-checkout/ ";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/project-checkout/";
 	$github = "https://github.com/TobyAbel/project-checkout";		//Github project location.
 	$lastVersionFile = "/home/www-data/project-checkout/lastversion.php";
+	$homeubuntu = "/home/ubuntu/";
 ?>

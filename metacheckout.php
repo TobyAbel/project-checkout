40923380ebb651458b78e09a6facacb792b05889
peterbrescia <peter.brescia@outlook.com>
2013-09-27 12:00:18 +0100

    16

--- metacheckout.php
+++ metacheckout.php
@@ -3,7 +3,7 @@
 require_once "metacheckoutconfig.php";
 require_once "checkoutconfig.php";
 
-Print "placeholder";
+$message = "placeholder";
 if (file_exists($lastVersionFile)) {
   $lastver = file_get_contents($lastVersionFile); // Get previous file version
 } else {
@@ -14,13 +14,13 @@ if (file_exists($lastVersionFile)) {
 
 //shell_exec("eval `ssh-agent`; ssh-keygen -t rsa -C $email");
 
-$message = 'Last ver = '.$lastver;
+$message .= 'Last ver = '.$lastver;
 if (isset($_POST['version']) && isset($_POST['password'])) {
   $ver = $_POST['version'];
   $pas = $_POST['password'];
 
   if ($checkoutPassword == $pas) {
-      $message  = 'Boom.'.PHP_EOL.'Last ver = '.$lastver.'<br />';
+      $message  .= 'Boom.'.PHP_EOL.'Last ver = '.$lastver.'<br />';
       $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git fetch github ".$ver.":".$ver." -v 2>&1; git --work-tree=".$projectWorkingDirectory." checkout -f ".$ver." 2>&1";
       $output = shell_exec($commands);
       $message .= str_replace(';', ';'.PHP_EOL, $commands).PHP_EOL;

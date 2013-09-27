e6d1efeeb42a9a76e7c1f1ef935fd9d9cd6f17d1
peterbrescia <peter.brescia@outlook.com>
2013-09-27 15:58:25 +0100

    ccc

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,10 +1,10 @@
 <?php
 	$checkoutPassword = 111;
-	$githubKey = "/home/www-data/.ssh/clonerepokey";
+	$githubKey = "/home/www-data/.ssh/clonekey";
 	$projectGithubDirectory = "/home/www-data/clonedirectory/";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/clonedirectory/";
 	$github = "git@github.com:peterbrescia/clonerepo.git";		//Github project location.
 	$directory = "/home/www-data/clonedirectory/";
 	$lastVersionFile = "/home/www-data/clonedirectory/lastversion.php";
-	$githubkeyorigin = "/var/www/project-checkout/clonerepokey";
+	$githubkeyorigin = "/var/www/project-checkout/clonekey";
 ?>

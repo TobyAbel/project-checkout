118fc76881101f550713823afc846ce747ac1baa
peterbrescia <peter.brescia@outlook.com>
2013-09-27 11:55:41 +0100

    13

--- metacheckout.php
+++ metacheckout.php
@@ -13,14 +13,14 @@ if (file_exists($lastVersionFile)) {
 }
 
 //shell_exec("eval `ssh-agent`; ssh-keygen -t rsa -C $email");
-
-$message = 'Last ver = '.$lastver;
+$message = 'Placeholder -1';
+$message .= 'Last ver = '.$lastver;
 if (isset($_POST['version']) && isset($_POST['password'])) {
   $ver = $_POST['version'];
   $pas = $_POST['password'];
 
   if ($checkoutPassword == $pas) {
-      $message  = 'Boom.'.PHP_EOL.'Last ver = '.$lastver.'<br />';
+      $message .= 'Boom.'.PHP_EOL.'Last ver = '.$lastver.'<br />';
       $commands = "cd ".$projectGithubDirectory." 2>&1; eval `ssh-agent`; ssh-add ".$githubKey." 2>&1; git fetch github ".$ver.":".$ver." -v 2>&1; git --work-tree=".$projectWorkingDirectory." checkout -f ".$ver." 2>&1";
       $message .= 'Placeholder 0';
       $output = shell_exec($commands);

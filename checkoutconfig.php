9643635b214070edd3d29d1657ec69e72d5ab356
peterbrescia <peter.brescia@outlook.com>
2013-10-01 16:52:36 +0100

    test 4

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -6,7 +6,7 @@
 
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/$keyname";
-	$projectGithubDirectory = "/home/www-data/$reponame";		//Project github directory
+	$projectGithubDirectory = "/home/www-data/$reponame/";		//Project github directory
 	$github = "git@github.com:peterbrescia/$reponame.git";		//Github project location.
 	$lastVersionFile = "/home/www-data/$reponame/lastversion.php";
 	$githubkeyorigin = "/var/www/project-checkout/$keyname";

2127c66bac6256726971eb75af0ab5ad6a7cac65
peterbrescia <peter.brescia@outlook.com>
2013-10-01 10:46:43 +0100

    Unicorn 3

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -4,13 +4,13 @@
 	$webroot = "wordpress/wp-content/themes/unicorn/";
 	$keyname = "clonekey";
 	//End Editable variables.
-	
+
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/$keyname";
 	$projectGithubDirectory = "/home/www-data/$reponame";		//Producct github directory
 	$projectWorkingDirectory = "/var/www/$webroot";
 	$github = "git@github.com:peterbrescia/$reponame.git";		//Github project location.
-	$directory = "/home/www-data/$repopname/";
+	$directory = "/home/www-data/$reponame/";
 	$lastVersionFile = "/home/www-data/$reponame/lastversion.php";
 	$githubkeyorigin = "/var/www/project-checkout/$keyname";
 ?>

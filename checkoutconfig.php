4679a0712d98492a1910473bfb0f083d43130319
peterbrescia <peter.brescia@outlook.com>
2013-10-01 10:32:35 +0100

    Unicorn 1

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,10 +1,10 @@
 <?php
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/clonekey";
-	$projectGithubDirectory = "/home/www-data/clonedirectory/";		//Producct github directory
-	$projectWorkingDirectory = "/var/www/clonedirectory/";
-	$github = "git@github.com:peterbrescia/clonerepo.git";		//Github project location.
-	$directory = "/home/www-data/clonedirectory/";
-	$lastVersionFile = "/home/www-data/clonedirectory/lastversion.php";
+	$projectGithubDirectory = "/home/www-data/wordpress/wp-content/themes/unicorn/";		//Producct github directory
+	$projectWorkingDirectory = "/var/www/wordpress/wp-content/themes/unicorn/";
+	$github = "git@github.com:peterbrescia/unicorn.git";		//Github project location.
+	$directory = "/home/www-data/wordpress/wp-content/themes/unicorn/";
+	$lastVersionFile = "/home/www-data/wordpress/wp-content/themes/unicorn/lastversion.php";
 	$githubkeyorigin = "/var/www/project-checkout/clonekey";
 ?>

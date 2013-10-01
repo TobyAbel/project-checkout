d6207f76d5529d529eda6e0a7609f6e78890b170
peterbrescia <peter.brescia@outlook.com>
2013-10-01 15:49:57 +0100

    test 1

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,17 +1,16 @@
 <?php
 	//Edit these only!
-	$reponame = "unicorn";
-	$webroot = "wordpress/wp-content/themes/unicorn/";
+	$reponame = "testing";
 	$keyname = "clonekey";
 	//End Editable variables.
 
 	$checkoutPassword = 111;
 	$githubKey = "/home/www-data/.ssh/$keyname";
 	$projectGithubDirectory = "/home/www-data/$reponame";		//Producct github directory
-	$projectWorkingDirectory = "/var/www/$webroot";
 	$github = "git@github.com:peterbrescia/$reponame.git";		//Github project location.
 	$directory = "/home/www-data/$reponame/";
 	$remotename = $reponame;
 	$lastVersionFile = "/home/www-data/$reponame/lastversion.php";
 	$githubkeyorigin = "/var/www/project-checkout/$keyname";
+	$projectWorkingDirectory = "/var/git-working-directory/";
 ?>

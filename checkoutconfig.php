aea0cadec0d57a48163e611f4df1787633a82d05
peterbrescia <peter.brescia@outlook.com>
2013-10-01 10:44:38 +0100

    Unicorn 2

--- checkoutconfig.php
+++ checkoutconfig.php
@@ -1,10 +1,16 @@
 <?php
+	//Edit these only!
+	$reponame = "unicorn";
+	$webroot = "wordpress/wp-content/themes/unicorn/";
+	$keyname = "clonekey";
+	//End Editable variables.
+	
 	$checkoutPassword = 111;
-	$githubKey = "/home/www-data/.ssh/clonekey";
-	$projectGithubDirectory = "/home/www-data/wordpress/wp-content/themes/unicorn/";		//Producct github directory
-	$projectWorkingDirectory = "/var/www/wordpress/wp-content/themes/unicorn/";
-	$github = "git@github.com:peterbrescia/unicorn.git";		//Github project location.
-	$directory = "/home/www-data/wordpress/wp-content/themes/unicorn/";
-	$lastVersionFile = "/home/www-data/wordpress/wp-content/themes/unicorn/lastversion.php";
-	$githubkeyorigin = "/var/www/project-checkout/clonekey";
+	$githubKey = "/home/www-data/.ssh/$keyname";
+	$projectGithubDirectory = "/home/www-data/$reponame";		//Producct github directory
+	$projectWorkingDirectory = "/var/www/$webroot";
+	$github = "git@github.com:peterbrescia/$reponame.git";		//Github project location.
+	$directory = "/home/www-data/$repopname/";
+	$lastVersionFile = "/home/www-data/$reponame/lastversion.php";
+	$githubkeyorigin = "/var/www/project-checkout/$keyname";
 ?>
